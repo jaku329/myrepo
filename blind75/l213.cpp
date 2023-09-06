bd33ee4e131b168596921917cce3a04d3f02220f
thomasshen <thomas.shen@mic.com.tw>
2023-09-06 09:40:02 +0800

    leetcode 213

---
+++ blind75/l213.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/house-robber-ii
+
+class Solution {
+public:
+  int rob(vector<int>& nums) {
+    if (nums.size() == 1) return nums[0];
+    vector<int> dp1(nums.size(), 0);
+    vector<int> dp2(nums.size(), 0);
+    dp1[0] = nums[0];
+    dp1[1] = max(dp1[0], nums[1]);
+    for (int i = 2; i < nums.size() - 1; ++i) {
+      dp1[i] = max(dp1[i - 1], dp1[i - 2] + nums[i]);
+    }
+
+    dp2[0] = 0;
+    dp2[1] = nums[1];
+    for (int i = 2; i < nums.size(); ++i) {
+      dp2[i] = max(dp2[i - 1], dp2[i - 2] + nums[i]);
+    }
+    return max(dp1[nums.size() - 2], dp2[nums.size() - 1]);
+  }
+};
