b8efd5188558f6e4e631d464c2ec6d0cded04f5a
thomasshen <thomas.shen@mic.com.tw>
2023-10-20 09:49:35 +0800

    leetcode 876

---
+++ grind75/l876.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+class Solution {
+public:
+  ListNode* middleNode(ListNode* head) {
+    ListNode* fast = head;
+    ListNode* slow = head;
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    return slow;
+  }
+};
