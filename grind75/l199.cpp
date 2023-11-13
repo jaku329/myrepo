4827f8ef48e3456781f32ac5c11998f30fa9efb8
thomasshen <thomas.shen@mic.com.tw>
2023-11-13 15:21:20 +0800

    leetcode 199

---
+++ grind75/l199.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/binary-tree-right-side-view/
+
+class Solution {
+public:
+  vector<int> rightSideView(TreeNode* root) {
+    vector<int> result;
+    if (root == NULL) return result;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+      int size = q.size();
+      for (int i = 0; i < size; ++i) {
+        TreeNode* n = q.front();
+        q.pop();
+
+        if (i = size - 1)
+          result.push_back(n->val);
+
+        if (n->left) q.push(n->left);
+        if (n->right) q.push(n->right);
+      }
+    }
+    return result;
+  }
+};
