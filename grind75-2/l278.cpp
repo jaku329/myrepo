2b1dbe556a9dcbbc26dc4cbfe1fc36513fb64210
jaku329 <jaku329@gmail.com>
2023-12-16 15:52:01 +0800

    Create l278.cpp

---
+++ grind75-2/l278.cpp
@@ -0,0 +1,14 @@
+// 278. First Bad Version
+
+int firstBadVersion(int n) {
+  int l = 1;
+  int r = n;
+  while (l < r) {
+    int m = l + ((r - l) >> 2);
+	if (isBadVersion(m))
+	  r = m;
+	else
+	  l = m + 1;
+  }
+  return l;
+}
