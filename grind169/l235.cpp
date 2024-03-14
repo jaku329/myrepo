826497d5494a1141fae7f9c0cf839a3fb0ce234c
thomasshen <thomas.shen@mic.com.tw>
2024-03-14 11:55:45 +0800

    some grind169

--- grind169/l235.cpp
+++ grind169/l235.cpp
@@ -1,13 +1,12 @@
 // 235. Lowest Common Ancestor of a Binary Search Tree
 
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-  if (root == NULL) return NULL;
+  if (root == NULL || p == NULL || q == NULL) return NULL;
   else if (root == p || root == q) return root;
-
-  TreeNode* lChild = lowestCommonAncestor(root->left, p, q);
-  TreeNode* rChild = lowestCommonAncestor(root->right, p, q);
-  if (lChild && rChild) return root;
-  else if (lChild) return lChild;
-  else if (rChild) return rChild;
-  return NULL;
+  
+  if (root->val < p->val && root->val < q->val)
+    return lowestCommonAncestor(root->right, p, q);
+  else if (root->val > p->val && root->val > q->val)
+    return lowestCommonAncestor(root->left, p, q);
+  return root;
 }
